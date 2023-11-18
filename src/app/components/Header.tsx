c39c385a0e0784f1edbf92821de3d0d836d92673
tawan <thiago_tawan@outlook.com>
2023-11-18 09:21:46 -0800

    album

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,14 +3,20 @@ import Image from "next/image"
 
 export default function Header () {
     return (
-        <header className="flex justify-center items-center text-center left-0 rigth-0 fixed bg-white w-full top-0 p-4 ">
-         <h1 className="text-xl text-center  bold">
+        <header className="flex items-center text-center justify-between fixed bg-white w-full top-0 p-4 ">
+        <div className="flex items-center ">
+        <h1 className="text-sx text-center  bold">
           Camping do Gnomo <span className="text-blue-900 italic">Mistico</span>
         </h1>
-        <Image className="w-12 h-12 ml-4 "
-        src={gnomo}
-        alt="gnomo icon"
-        />
+
+     
+        </div>
+
+      
+            
+           <div className="">
+           <button className="bg-red-600 text-white rounded p-2 w-32 hover:bg-red-500 transition">Album</button>
+           </div>
         </header>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import { TextOne, TextTwo, InstagramButton } from "./components/Texts";
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center ml-8 mr-8 text-center pb-32 mt-12">
+       <Header/>
       <div className="max-w-sm">
-        <Header/>
         <Image
           className="mt-16 object-cover object-center  w-full h-64 rounded"
           src={nigth}
