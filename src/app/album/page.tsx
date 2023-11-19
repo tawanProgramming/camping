33a94726db7438c24c5d04980c1a0904f7831342
tawan <thiago_tawan@outlook.com>
2023-11-19 12:35:38 -0300

    😯

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -20,7 +20,7 @@ export default function Album () {
 
         <ul className="flex justify-center items-center text-center gap-4 flex-wrap  max-w-4xl">
         {photos.map((photo) => (
-          <li key={photo.id}>
+          <li key={photo._id}>
             <img src={photo.photo_url} className="rounded w-64 h-64 object-cover" alt={photo.title} />
             <h2 className="font-bold mt-3">{photo.title}</h2>
             <p className="text-gray-500">{photo.description}</p>
