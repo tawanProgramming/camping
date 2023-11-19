a70121eb0669f8af6ee797944bc78bd28acbc5d2
tawan <thiago_tawan@outlook.com>
2023-11-19 10:01:00 -0300

    api to photos

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -1,7 +1,33 @@
+"use client"
+import axios from "axios"
+import {useState, useEffect} from "react"
 export default function Album () {
+    const [photos, setPhotos] = useState([])
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                const response = await axios.get("http://localhost:3100")
+                setPhotos(response.data)
+            } catch (err) {
+                console.error("error" + err)
+            }
+        }
+        fetch()
+    }, [])
     return (
-       <div className="mt-36 flex justify-center">
+       <div className="mt-36 items-center flex gap-5 flex-col justify-center">
         <h1 className="text-red-700 font-bold inika">Fotos</h1>
+
+        <ul className="flex justify-center items-center text-center gap-4 flex-wrap  max-w-4xl">
+        {photos.map((photo) => (
+          <li key={photo.id}>
+            <img src={photo.photo_url} className="rounded w-64 h-64 object-cover" alt={photo.title} />
+            <h2 className="font-bold mt-3">{photo.title}</h2>
+            <p className="text-gray-500">{photo.description}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="mb-32"></div>
        </div>
     )
 }
